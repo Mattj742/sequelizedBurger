922315e9f26908f7e61c81289fee8bf6ad147589
Matthew Johnson <mattj742@gmail.com>
2017-06-02 12:04:03 -0400

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Black Bean Burger");
+INSERT INTO burgers (burger_name) VALUES ("Classic Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Deluxe Double Cheeseburger");
